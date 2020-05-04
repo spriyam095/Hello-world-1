2ba8ccb812b74c7f0a94795ff12c1343b439b617
aniruddha27 <b.aniruddha27@gmail.com>
2020-05-04 15:25:10 +0530

    Adding for-loop to java file

--- Java/HelloJava.java
+++ Java/HelloJava.java
@@ -4,5 +4,8 @@ class HelloJava
     public static void main(String args[]) 
     { 
         System.out.println("Hello world from Java!"); 
+     
+        for (int i = 0; i <= 2; i++) 
+            System.out.println("Nice to meet you!"); 
     } 
 } 
