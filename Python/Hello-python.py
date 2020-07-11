4cd2c42003522d6a7987496e73e507d35f9d890d
Karthik Swaminathan <karthiks1983@gmail.com>
2020-07-11 17:41:51 -0400

    Hello Python program update

--- Python/Hello-python.py
+++ Python/Hello-python.py
@@ -1,5 +1,7 @@
+import math;
 print("Hello world from Python!")
 print("Sorry, hello world from Python3!")
 for i in range(3):
 	print("Nice to meet you!")
 	print("same here")
+print (math.sqrt(25))
